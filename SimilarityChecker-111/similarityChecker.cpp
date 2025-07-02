8f9a788520080abeadb5d6019f5999030b79bf7a
Yerim <yryun94@gmail.com>
2025-07-02 15:38:38 +0900

    add same legth tc

--- SimilarityChecker-111/similarityChecker.cpp
+++ SimilarityChecker-111/similarityChecker.cpp
@@ -1,3 +1,14 @@
+#include <string>
+
+using std::string;
+
 class Checker {
+public:
+	int getLengthPoints(const string& str1, const string& str2) {
+		if (str1.length() == str2.length())
+			return 60;
+		else
+			return 0;
+	}
 
 };
--- SimilarityChecker-111/similarityChecker_test.cpp
+++ SimilarityChecker-111/similarityChecker_test.cpp
@@ -3,8 +3,13 @@
 
 using namespace testing;
 
-TEST(TG, TC1) {
-	EXPECT_EQ(1, 1);
+TEST(TG, TCSameLength) {
+	Checker checker;
+	string str1 = "ABCDE";
+	string str2 = "EDCBA";
+	
+	int actual = checker.getLengthPoints(str1, str2);
+	EXPECT_EQ(60, actual);
 }
 
 int main() {
