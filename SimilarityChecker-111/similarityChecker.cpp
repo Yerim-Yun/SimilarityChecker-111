e8be224e0d6b0feca28e7e5cdd020ca048d65abe
Yerim <yryun94@gmail.com>
2025-07-02 16:51:16 +0900

    add same alpha tc

--- SimilarityChecker-111/similarityChecker.cpp
+++ SimilarityChecker-111/similarityChecker.cpp
@@ -1,11 +1,20 @@
 #include <string>
+#include <map>
 
 using std::string;
+using std::map;
 
 class Checker {
 public:
 	Checker(const string& str1, const string& str2)
-		: str1(str1), str2(str2) {}
+		: str1(str1), str2(str2)
+	{
+		for (char c : str1)
+			strmap1[c]++;
+		
+		for (char c : str2)
+			strmap2[c]++;
+	}
 
 	int getLengthPoints() {
 		int lengthStr1 = str1.length();
@@ -22,6 +31,21 @@ public:
 			return getPartialPoints(gap, minLength);
 		}
 	}
+
+	int getAlphaPoints() {
+		int alphaCnt1 = strmap1.size();
+		int alphaCnt2 = strmap2.size();
+		int sameAlphaCnt = 0;
+
+		for (auto targetStr : strmap1) {
+			if (targetStr.second != strmap2[targetStr.first]) continue;
+				sameAlphaCnt++;
+		}
+
+		if (sameAlphaCnt == alphaCnt1) return 40;
+		else return 0;
+	}
+
 private:
 	int getPartialPoints(int gap, int minLeng) {
 		return (1 - (double)gap / minLeng) * 60;
@@ -29,6 +53,11 @@ private:
 	const int MAX_LENGTH_POINT = 60;
 	const int MIN_LENGTH_POINT = 0;
 
+	const int MAX_ALPHA_POINT = 40;
+	const int MIN_ALPHA_POINT = 0;
+
+	map<char, int> strmap1;
+	map<char, int> strmap2;
 	string str1;
 	string str2;
 };
--- SimilarityChecker-111/similarityChecker_test.cpp
+++ SimilarityChecker-111/similarityChecker_test.cpp
@@ -5,23 +5,33 @@ using namespace testing;
 
 class CheckerFixture :public testing::Test {
 public:
-	void check(int expected, string str1, string str2) {
+	void checkLength(int expected, string str1, string str2) {
 		Checker checker{ str1 , str2 };
 		int actual = checker.getLengthPoints();
+		EXPECT_EQ(expected, actual);
+	}
+
+	void checkAlpha(int expected, string str1, string str2) {
+		Checker checker{ str1 , str2 };
+		int actual = checker.getAlphaPoints();
 		EXPECT_EQ(expected, actual);
 	}
 };
 
 TEST_F(CheckerFixture, TCSameLength) {
-	check(60, "ABCDE" , "EDCBA");
+	checkLength(60, "ABCDE" , "EDCBA");
 }
 
 TEST_F(CheckerFixture, TCDiffDoubleLength) {
-	check(0, "ABCD" , "ED");
+	checkLength(0, "ABCD" , "ED");
 }
 
 TEST_F(CheckerFixture, TCDiffPartialLength) {
-	check(20, "AAABB" , "BAA");
+	checkLength(20, "AAABB" , "BAA");
+}
+
+TEST_F(CheckerFixture, TCSameAlpha) {
+	checkAlpha(40, "ASD", "DSA");
 }
 
 int main() {
